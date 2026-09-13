387d5e41b7efffef6f49c25b3cbe852b6b447006
justdeepanshugupta <justdeepanshugupta@gmail.com>
2026-09-13 19:40:55 +0530

    Add files via upload

---
+++ Program2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+// This is my second program in which i use Arithmatic Operators
+
+int main(){
+    int a = 10 , b = 5;
+
+    cout << "Sum: " << (a + b) << endl;
+    cout << "Difference: " << (a - b) << endl;
+    cout << "Product: " << (a * b) << endl;
+    cout << "Division: " << (a / b) << endl;
+
+    return 0;
+}

5967f841bc52879cd48ce90a2d38e10c1ee44968
justdeepanshugupta <justdeepanshugupta@gmail.com>
2026-09-13 19:29:35 +0530

    Add files via upload

---
+++ Program1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cout << "Enter Your age: ";
+    cin >> age;
+
+    cout << "Your age is: " << age << endl;
+    return 0;
+}

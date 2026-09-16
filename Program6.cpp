e85b87f2b7bc35ab431725197e880968d6fe9e48
justdeepanshugupta <justdeepanshugupta@gmail.com>
2026-09-16 21:33:51 +0530

    Add files via upload

---
+++ Program6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n = 25;
+
+    if (n >= 0){
+        cout << "n is positive\n";
+    } else {
+        cout << "n is negative";
+    }
+    return 0;
+}

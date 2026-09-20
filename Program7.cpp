3e0557947b608c670dac8f16f90671632215ffb6
justdeepanshugupta <justdeepanshugupta@gmail.com>
2026-09-20 20:12:18 +0530

    Add files via upload

---
+++ Program7.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int marks;
+    cout << "Enter Your Marks: ";
+    cin >> marks;
+
+    if (marks >= 90) {
+        cout << "A\n";
+    
+    } else if (marks >= 80 && marks < 90) {
+        cout << "B\n";
+    } else {
+        cout << "C\n";
+    }
+    return 0;
+}

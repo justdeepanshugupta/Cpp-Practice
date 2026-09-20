62d11029f0b2c708f9514aea1c69d0ddba488230
justdeepanshugupta <justdeepanshugupta@gmail.com>
2026-09-20 20:12:48 +0530

    Add files via upload

---
+++ Program8.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+//Practice Question 1
+int main() {
+    char ch;
+
+    cout << "Enter a char: ";
+    cin >> ch;
+
+    if (ch >= 'a' && ch <= 'z'){
+        cout << "Lowercase\n";
+    } else {
+        cout << "Uppercase\n";
+    }
+
+    return 0;
+}
---
+++ Program9.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+// Ternary Operator
+int main(){
+    int n = 45;
+
+    cout << (n >= 0 ? "Positive" : "Negative") << endl;
+
+    return 0;
+}

18d1813eb764b03658bb7b8e4125671b194d9abf
justdeepanshugupta <justdeepanshugupta@gmail.com>
2026-09-23 23:04:09 +0530

    Add files via upload

---
+++ Program10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+//while loop.
+
+int main(){
+    int n = 50;
+    int count = 1;
+    while(count <= n){
+    cout << count;
+    count++;
+    }
+cout << endl;
+return 0;
+}
+
